e3ca9aeed81cc58d52baaed589276c31ef675a0e
jthatcher1293 <jthatcher1293@gmail.com>
2018-11-16 09:45:00 -0600

    add aliases

---
+++ aliases.sh
@@ -0,0 +1,20 @@
+alias py2="$HOME/.pyenv/versions/2.7.14/bin/python"
+alias py36="$HOME/.pyenv/versions/3.6.5/bin/python"
+alias py34="$HOME/.pyenv/versions/3.4.8/bin/python"
+alias mkvenv27="mkvirtualenv $1 -p$HOME/.pyenv/versions/2.7.14/bin/python"
+alias mkvenv34="mkvirtualenv $1 -p$HOME/.pyenv/versions/3.4.8/bin/python"
+alias mkvenv36="mkvirtualenv $1 -p$HOME/.pyenv/versions/3.6.5/bin/python"
+alias which-tmux='tmux display-message -p "#S"'
+alias now='date +%a%t%m/%d/%Y%t%H:%M:%S\ %z\(%Z\)'
+alias tree="tree -I '*.pyc|__pycache__'"
+alias ts="tig status"
+alias gcb="git checkout -b"
+alias pr="hub pull-request"
+alias gs='git status'
+alias gd='git diff'
+alias gc='git commit'
+alias push='git publish'
+alias pull='git pull-rebase'
+alias delete_pyc='find . -name \*.pyc -exec rm \{\} \+'
+alias gcb='git checkout -b'
+alias eft='ember exam --split=2 --parallel --silent'
