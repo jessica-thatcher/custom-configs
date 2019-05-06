dffae5e2e86c057b60f2b117634859ab827abcd8
jwilson1293 <jthatcher1293@gmail.com>
2019-05-06 13:06:48 -0500

    add additional aliases

--- aliases.sh
+++ aliases.sh
@@ -18,3 +18,10 @@ alias pull='git pull-rebase'
 alias delete_pyc='find . -name \*.pyc -exec rm \{\} \+'
 alias gcb='git checkout -b'
 alias eft='ember exam --split=2 --parallel --silent'
+alias define='PYTHONPATH=$HOME/.virtualenvs/dictionary/lib/python3.6/site-packages $HOME/.virtualenvs/dictionary/bin/python $HOME/.virtualenvs/dictionary/bin/dict.py'
+alias vector-staging-deploy='git checkout master; pull; bundle; bundle exec cap staging deploy'
+alias rm-eft='rm out && eft > out'
+alias py2="$HOME/.pyenv/versions/2.7.15/bin/python"
+alias py36="$HOME/.pyenv/versions/3.6.8/bin/python"
+alias py34="$HOME/.pyenv/versions/3.4.9/bin/python"
+alias py37="$HOME/.pyenv/versions/3.7.2/bin/python"
